0c0641a61c864bf23936de757a0020f6efd6390d
ishanrai05 <ishanrai05@gmail.com>
2019-10-25 17:11:35 +0530

    Add filters

--- transdb/admin.py
+++ transdb/admin.py
@@ -16,21 +16,73 @@ from .models import (
     PtSurveyRating
     )
 
-class TransDBAdmin(admin.ModelAdmin):
-    pass
+
+admin.site.site_header = 'TSaaS Admin Dashboard'
+
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('memberID', 'familyID', 'created_at')
+    list_filter = ('gender','monthlyIncome','householdHead')
+
 
 class FamilyDBAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('familyID', 'collegeID', 'noOfMembers')
+    list_filter = ('collegeID', 'noOfMembers', 'familyIncome', 'homeState', 'nameOfDistrict')
 
-admin.site.register(SurveyList)
-admin.site.register(SurveyType)
-admin.site.register(ResponseTime)
-admin.site.register(CollegeList)
-admin.site.register(Family)
-admin.site.register(Member)
-admin.site.register(Trip)
-admin.site.register(OriginDestination)
-admin.site.register(Mode)
-admin.site.register(Feedback)
-admin.site.register(PtSurvey)
-admin.site.register(PtSurveyRating)
+
+class SurveyListAdmin(admin.ModelAdmin):
+    list_display = ('surveyID', 'surveyType')
+    list_filter = ('surveyType',)
+
+
+class SurveyTypeAdmin(admin.ModelAdmin):
+    list_display = ('surveyTypeID', 'surveyFormat')
+    list_filter = ('surveyFormat',)
+
+
+class ResponseTimeAdmin(admin.ModelAdmin):
+    list_display = ('responseTimeID', 'surveyID', 'surveyStartTime', 'surveyEndTime')
+
+
+class CollegeListAdmin(admin.ModelAdmin):
+    list_display = ('collegeID', 'collegeName', 'surveyTypeID')
+    list_filter = ('surveyTypeID',)
+
+
+class TripAdmin(admin.ModelAdmin):
+    list_display = ('tripID', 'memberID')
+
+
+class ODAdmin(admin.ModelAdmin):
+    list_display = ('originDestinationID', 'tripID', 'travelDistance', 'fare')
+
+
+class ModeAdmin(admin.ModelAdmin):
+    list_display = ('modeID', 'tripID', 'modeName')
+
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('id','feedback_time',)
+
+
+class PtSurveyAdmin(admin.ModelAdmin):
+    list_display = ('personID', 'collegeID', 'surveyID')
+    list_filter = ('gender', 'monthlyIncome')
+
+
+class PtSurveyRatingAdmin(admin.ModelAdmin):
+    list_display = ('ptSurveyRatingId', 'personID',)
+
+
+admin.site.register(SurveyList, SurveyListAdmin)
+admin.site.register(SurveyType, SurveyTypeAdmin)
+admin.site.register(ResponseTime, ResponseTimeAdmin)
+admin.site.register(CollegeList, CollegeListAdmin)
+admin.site.register(Family, FamilyDBAdmin)
+admin.site.register(Member, MemberAdmin)
+admin.site.register(Trip, TripAdmin)
+admin.site.register(OriginDestination, ODAdmin)
+admin.site.register(Mode, ModeAdmin)
+admin.site.register(Feedback, FeedbackAdmin)
+admin.site.register(PtSurvey, PtSurveyAdmin)
+admin.site.register(PtSurveyRating, PtSurveyRatingAdmin)
